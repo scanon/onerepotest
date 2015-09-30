0bd51c092683fdbf5dc5b9ca9108e01ad3a00614
Roman Sutormin <rsutormin@lbl.gov>
2015-09-30 12:13:39 -0700

    Python service startup host was fixed.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,6 +4,6 @@ export PYTHONPATH=/kb/deployment/lib:$PYTHONPATH
 export KB_DEPLOYMENT_CONFIG=$script_dir/../deploy.cfg
 WORK_DIR=/kb/deployment/services/onerepotest
 cd $WORK_DIR
-python /kb/deployment/lib/OnerepotestServer.py --host localhost --port 5000
+python /kb/deployment/lib/OnerepotestServer.py --host 0.0.0.0 --port 5000
 # >$WORK_DIR/py_server.out 2>$WORK_DIR/py_server.err
 # echo $pid > $WORK_DIR/pid.txt
